319d53a5160c5e93b9a23abfe4deaddb043a3bd4
zemendaniel <zemendaniel@gmail.com>
2025-11-25 10:03:37 +0100

    update api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,4 +7,20 @@ const api = axios.create({
   },
 });
 
+// Add a request interceptor
+api.interceptors.request.use(
+  (config) => {
+    // Check both storages for the token
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
