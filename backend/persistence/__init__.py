c9d6bce211497ef25396a8236ea887fe18086f71
szabo <szaboboernat06@gmail.com>
2025-11-25 10:08:39 +0100

    user(felkesz), repository

--- backend/persistence/__init__.py
+++ backend/persistence/__init__.py
@@ -1,17 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from typing import Annotated
+
+from sqlmodel import Session, SQLModel, create_engine
+
 from ..config import Config
 
-engine = create_engine(str(Config.SQL_ALCHEMY_DATABASE_URL))
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class Base(DeclarativeBase):
-    pass
+connect_args = {"check_same_thread": False}
+engine = create_engine(str(Config.SQL_ALCHEMY_DATABASE_URL), connect_args=connect_args)
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
--- backend/persistence/model/user.py
+++ backend/persistence/model/user.py
@@ -1,14 +1,50 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean, DateTime
-from backend.persistence import Base
+from uuid import uuid4
+
+from sqlalchemy import String
+from sqlmodel import Field, SQLModel
 from datetime import datetime, timezone
 
+from werkzeug.security import check_password_hash, generate_password_hash
 
-class User(Base):
-    __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(128), nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
-    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: str = Field(
+        default_factory=lambda: str(uuid4()),
+        sa_type=String(64),
+        primary_key=True,
+    )
+
+    email: str = Field(
+        sa_type=String(128),
+        unique=True,
+        nullable=False,
+    )
+
+    # hidden from API responses
+    _password: str = Field(
+        sa_type=String(128),
+        nullable=False,
+        exclude=True,
+    )
+
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)},
+        nullable=False,
+    )
+
+    # todo machek, profile, role
+
+    @_password.setter
+    def password(self, value):
+        self._password = generate_password_hash(value)
+
+    def check_password(self, password) -> bool:
+        return check_password_hash(self._password, password)
