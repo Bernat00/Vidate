161aee9b8e9a48ae8e4358037fc0b0f28f6573c4
szabo <szaboboernat06@gmail.com>
2026-01-06 13:12:50 +0100

    redis

--- backend/routes/realtime/__init__.py
+++ backend/routes/realtime/__init__.py
@@ -5,30 +5,18 @@ from typing import Any
 from fastapi import APIRouter, WebSocket, WebSocketException, status
 
 from backend.routes.auth import decode_token
-from backend.schemas.auth import TokenData
 
-
+import redis.asyncio as redis
 
 
 
 router = APIRouter(prefix='/ws')
 
 
-class Connection:
-    user_id: str
-    websocket: WebSocket
-
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[Connection] = []
-
-
-    def get_active_connection(self, websocket: WebSocket) -> Connection | None:
-        for conn in self.active_connections:
-            if conn.websocket == websocket:
-                return conn
-        return None
+        self.r = redis.Redis(host='localhost', port=6379)
 
 
     async def connect(self, websocket: WebSocket): #a szaros jsben nem lehet a ws-headert beallitani (asszem)
@@ -55,22 +43,18 @@ class ConnectionManager:
 
         token_data = decode_token(jwt, WebSocketException(code=status.WS_1008_POLICY_VIOLATION, reason='Client is not authenticated'))
 
-        conn = Connection()
-        conn.user_id = token_data.user_id
-        conn.websocket = websocket
+        #todo dani itt kezd
+        user_id = token_data.user_id
 
-        self.active_connections.append(conn)
 
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(self.get_active_connection(websocket))
+        pass
 
     async def send(self, data: Any, websocket: WebSocket):
         await websocket.send_json(data)
 
-    async def broadcast(self, data: Any):
-        for connection in self.active_connections:
-            await connection.websocket.send_json(data)
+
 
 
 manager = ConnectionManager()
