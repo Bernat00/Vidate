7f4f10fbc6dcf207ab616900f456defc178f80a0
szabo <szaboboernat06@gmail.com>
2026-01-23 14:13:40 +0100

    asd

--- backend/persistence/model/preferences/preferences.py
+++ backend/persistence/model/preferences/preferences.py
@@ -1,3 +1,4 @@
+from sqlalchemy import String
 from sqlmodel import Field, SQLModel
 from datetime import datetime, timezone
 
@@ -5,7 +6,16 @@ from datetime import datetime, timezone
 class Preference(SQLModel, table=True):
     __tablename__ = "preferences"
 
-    id: int = Field(primary_key=True)
+    user_id: str = Field(foreign_key="users.id", primary_key=True, sa_type=String(256))
+
+    first_name: str = Field(nullable=False)
+    middle_name: str = Field(nullable=True)
+    last_name: str = Field(nullable=False)
+    birth_date: datetime = Field(nullable=False)
+    gender_id: int = Field(foreign_key="genders.id", nullable=False)
+    language_id: int = Field(foreign_key="languages.id", nullable=False)
+    religion_id: int = Field(foreign_key="religions.id")
+
     wants_children: bool = Field(nullable=True)
 
 
--- backend/persistence/model/profile.py
+++ backend/persistence/model/profile.py
@@ -10,10 +10,10 @@ from datetime import datetime, timezone
 class Profile(SQLModel, table=True):
     __tablename__ = "profiles"
 
-    user_id: int = Field(foreign_key="users.id", primary_key=True)
+    user_id: str = Field(foreign_key="users.id", primary_key=True, sa_type=String(256))
 
     first_name: str = Field(nullable=False)
-    middle_name: str = Field(nullable=False)
+    middle_name: str = Field(nullable=True)
     last_name: str = Field(nullable=False)
     birth_date: datetime = Field(nullable=False)
     gender_id: int = Field(foreign_key="genders.id", nullable=False)
