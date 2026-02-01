e2da3fadc574c071f344a96cab2dde24b663f5a6
zemendaniel <zemen.daniel_2021c@bolyai-szakkozep.hu>
2026-02-01 19:17:20 +0100

    fix

--- frontend/src/components/layout/ChatColumn.tsx
+++ frontend/src/components/layout/ChatColumn.tsx
@@ -10,7 +10,7 @@ type ChatColumnProps = {
 export default function ChatColumn({ children }: ChatColumnProps): ReactElement {
   return (
     <div className="flex justify-center items-center h-full mx-2">
-      <div className="w-full lg:w-1/2 h-full">
+      <div className="w-full lg:w-4/5 h-full">
         <div className="flex flex-col h-full mx-2 pt-4">{children}</div>
       </div>
     </div>
