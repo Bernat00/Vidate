3a64c5334015e42c4dc155331aaf2e6e17baf756
szabo <szaboboernat06@gmail.com>
2026-02-17 08:51:56 +0100

    aaa

---
+++ VidateTests/VidateTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace VidateTests
+{
+
+    public class Tests
+    {
+        WebDriver driver;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://www.google.com";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Return);
+            Console.WriteLine(driver.Title);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Dispose();
+        }
+    }
+}
